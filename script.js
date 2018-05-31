fef98289ad3024d7691a053904ac44c8ce284fd5
Spencer Mathews <spencermathews@users.noreply.github.com>
2018-05-30 23:20:14 -0700

    Move leftover incomplete styling code into own function
    Currently this is the same as what is used to handle the state-level
    preview data, which styles the entire layer. We need to modify it to
    style each county feature since that data is accessed state by state.

--- script.js
+++ script.js
@@ -419,6 +419,50 @@ function selectStateListener(e) {
   // debugSelectEvent(e); // consider preserving use call() to preserve "this"
 }
 
+
+function styleCounties() {
+  var maxStories = 0;
+  for (let county of results) {
+    if (county.story_count > maxStories) {
+      maxStories = county.story_count;
+    }
+  }
+  console.log("maxStories (county):", maxStories);
+
+  // Sets style function for the layer
+  // Note this is still OK even if features have not been populated from source
+  countyLayer.setStyle(function (feature) {
+    let name = feature.get("name");
+    // console.log(name);
+    // style.getText().setText(name);
+    style.getText().setText("");
+
+    // SOMETIME find a more clever/efficient way of matching, maybe create a dict from results
+    for (let county of results) {
+      // set default color in case state fails to match
+      style.getFill().setColor(colors[3]);
+      if (name === county.name) {
+        // console.log(state.name, state.story_count, maxStories);
+        // is there a smarter way?
+        // TODO make actual quintile/quantile
+        // TODO where is Utah? make sure we match all
+        if (county.story_count > 9) {
+          style.getFill().setColor(colors[0]);
+        } else if (county.story_count > 6) {
+          style.getFill().setColor(colors[1]);
+        } else if (county.story_count > 3) {
+          style.getFill().setColor(colors[2]);
+        } else {
+          // this else may not be necessary since we set default above
+          style.getFill().setColor(colors[3]);
+        }
+        break;
+      }
+    }
+    return style;
+  });
+}
+
 function debugSelectEvent(e) {
   console.log('\nin debugSelectEvent:');
   // debug
@@ -631,49 +675,6 @@ function getStatePreview(state_name) {
       countyStories = arrayToObject(results, "name");
 
       return countyStories;
-
-      /* stuff below is ignored! */
-
-      var maxStories = 0;
-      for (let county of results) {
-        if (county.story_count > maxStories) {
-          maxStories = county.story_count;
-        }
-      }
-      console.log("maxStories (county):", maxStories);
-
-      // Sets style function for the layer
-      // Note this is still OK even if features have not been populated from source
-      countyLayer.setStyle(function (feature) {
-        let name = feature.get("name");
-        // console.log(name);
-        // style.getText().setText(name);
-        style.getText().setText("");
-
-        // SOMETIME find a more clever/efficient way of matching, maybe create a dict from results
-        for (let county of results) {
-          // set default color in case state fails to match
-          style.getFill().setColor(colors[3]);
-          if (name === county.name) {
-            // console.log(state.name, state.story_count, maxStories);
-            // is there a smarter way?
-            // TODO make actual quintile/quantile
-            // TODO where is Utah? make sure we match all
-            if (county.story_count > 9) {
-              style.getFill().setColor(colors[0]);
-            } else if (county.story_count > 6) {
-              style.getFill().setColor(colors[1]);
-            } else if (county.story_count > 3) {
-              style.getFill().setColor(colors[2]);
-            } else {
-              // this else may not be necessary since we set default above
-              style.getFill().setColor(colors[3]);
-            }
-            break;
-          }
-        }
-        return style;
-      });
     })
     .catch(function (err) {
       console.log(err);
